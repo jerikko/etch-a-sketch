0e1892a13efa5bb6d386e09b23ab495295c342ee
Jerikko Agatep <jerikkoagatep@gmail.com>
2020-05-12 21:20:47 -0700

    Add random color mouse-hover & remove borders

--- javascript.js
+++ javascript.js
@@ -4,5 +4,9 @@ for (i = 0; i < 16; i++) {
     const pixel = document.createElement('div');
     container.appendChild(pixel);
     pixel.setAttribute('class', 'pixel');
-    pixel.setAttribute('style', 'border-style: solid; border-color: black; border-width: 1px');
+    pixel.addEventListener('mouseenter', () => {
+        let randomColor = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+                            //  random hexadecimal color generator
+        pixel.setAttribute('style', `background-color: ${randomColor}`)
+    });
 };
